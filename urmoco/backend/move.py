40386b607ac279bd9d385431c2055379eaf0dec9
Emanuel Buholzer <emanuel0xb@gmail.com>
2022-09-16 15:39:42 +0200

    Timeouts should be able to occur

--- urmoco/backend/move.py
+++ urmoco/backend/move.py
@@ -50,14 +50,14 @@ def handle_move(config, state, robot: RobotClient, ur_out_q, df_out_q):
                     "current_frame": state["frame"]
                 }
             })
-    else:
-        state["move"]["time_elapsed_seconds"] += state["cycle"]["prev_duration_seconds"]
 
-        timeout_seconds = config.get('robot.move_timeout_seconds')
-        if state["move"]["time_elapsed_seconds"] > timeout_seconds:
-            robot.stop()
-            state["move"]["active"] = False
-            state["move"]["target_joints"] = None
-            state["move"]["time_elapsed_seconds"] = 0
-            ur_out_q.put({"type": "move_timeout"})
-            df_out_q.put({"type": "move_timeout"})
+    state["move"]["time_elapsed_seconds"] += state["cycle"]["prev_duration_seconds"]
+
+    timeout_seconds = config.get('robot.move_timeout_seconds')
+    if state["move"]["time_elapsed_seconds"] > timeout_seconds and not state["move"]["stopping"]:
+        robot.stop()
+        state["move"]["active"] = False
+        state["move"]["target_joints"] = None
+        state["move"]["time_elapsed_seconds"] = 0
+        ur_out_q.put({"type": "move_timeout"})
+        df_out_q.put({"type": "move_timeout"})
