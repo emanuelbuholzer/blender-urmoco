2c4adcc58f7df83203e0172604e556277612babb
emanuelbuholyer <emanuel0xb@gmail.com>
2023-02-16 13:44:55 +0100

    Discard Dragonframe move messages, when a move is already scheduled

--- urmoco/backend/dfmoco.py
+++ urmoco/backend/dfmoco.py
@@ -1,7 +1,10 @@
 def handle_dfmoco_request(dfmoco_req, state, robot, dfmoco_out_queue, urmoco_out_queue):
     if dfmoco_req["type"] == "move_to_frame":
-        state["move"]["scheduled"] = True
-        urmoco_out_queue.put(dfmoco_req)
+        if state["move"]["scheduled"]:
+            return
+        else:
+            state["move"]["scheduled"] = True
+            urmoco_out_queue.put(dfmoco_req)
 
     if dfmoco_req["type"] == "is_moving":
         dfmoco_out_queue.put(
